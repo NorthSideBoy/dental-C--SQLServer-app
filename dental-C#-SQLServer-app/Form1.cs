6ea3f21c4204b928d823a7c08a41966eeb75ffa8
Valenta001 <valentinafuentes42@gmail.com>
2025-01-25 23:27:52 -0500

    diseño de login, falta la parte funcional

--- dental-C#-SQLServer-app/Form1.cs
+++ dental-C#-SQLServer-app/Form1.cs
@@ -1,3 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+
 namespace dental_C__SQLServer_app
 {
     public partial class Form1 : Form
@@ -5,6 +18,75 @@ namespace dental_C__SQLServer_app
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void txtuser_Enter(object sender, EventArgs e)
+        {
+            //Condicion para vaciar el texto de usuario, para ingresar un nombre
+            if (txtuser.Text == "USUARIO")
+            {
+                txtuser.Text = "";
+                txtuser.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtuser_Leave(object sender, EventArgs e)
+        {
+            //Para que se vuelva a visualizar el texto:Usuario
+            if (txtuser.Text == "")
+            {
+                txtuser.Text = "USUARIO";
+                txtuser.ForeColor = Color.White;
+            }
+        }
+
+        private void txtpass_Enter(object sender, EventArgs e)
+        {
+            //Condicion para vaciar el texto de contraseña, e ingresar una contraseña
+            if (txtpass.Text == "CONTRASEÑA")
+            {
+                txtpass.Text = "";
+                txtpass.ForeColor = Color.LightGray;
+                txtpass.UseSystemPasswordChar = true; //Ocultar contraseña
+            }
+        }
+
+        private void txtpass_Leave(object sender, EventArgs e)
+        {
+            //Para que se vuelva a visualizar el texto
+            if (txtpass.Text == "")
+            {
+                txtpass.Text = "CONTRASEÑA";
+                txtpass.ForeColor = Color.White;
+                txtpass.UseSystemPasswordChar = false;
+            }
+
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnminimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnexpandir_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnmostrar_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+            //Mostrar y ocultar la contraseña que se ha ingresado
+            //txtpass.UseSystemPasswordChar = !UseSystemPasswordChar;
         }
     }
 }
