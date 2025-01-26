13027ca1db12b2e8f1233370ee2555e8ef84d6b4
RafaelR412 <rivasrosalesrafaelantonio@gmail.com>
2025-01-26 11:36:03 -0400

    Dashboard del programa en desarrollo (version:0.01), sujeto a posibles nuevos botones y a falta de un boton para hacer el dashboard pueda contraerse y desplegarse

--- dental-C#-SQLServer-app/Form1.Designer.cs
+++ dental-C#-SQLServer-app/Form1.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panelMenu = new Panel();
+            ptbLogo = new PictureBox();
+            btnConfig = new Button();
+            btnPacientes = new Button();
+            btnConsultas = new Button();
+            panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ptbLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(236, 255, 249);
+            panelMenu.Controls.Add(ptbLogo);
+            panelMenu.Controls.Add(btnConfig);
+            panelMenu.Controls.Add(btnPacientes);
+            panelMenu.Controls.Add(btnConsultas);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 519);
+            panelMenu.TabIndex = 0;
+            // 
+            // ptbLogo
+            // 
+            ptbLogo.BackColor = Color.FromArgb(14, 150, 143);
+            ptbLogo.Dock = DockStyle.Top;
+            ptbLogo.Image = (Image)resources.GetObject("ptbLogo.Image");
+            ptbLogo.Location = new Point(0, 0);
+            ptbLogo.Name = "ptbLogo";
+            ptbLogo.Size = new Size(220, 170);
+            ptbLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptbLogo.TabIndex = 1;
+            ptbLogo.TabStop = false;
+            // 
+            // btnConfig
+            // 
+            btnConfig.FlatAppearance.BorderSize = 0;
+            btnConfig.FlatStyle = FlatStyle.Flat;
+            btnConfig.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnConfig.Image = (Image)resources.GetObject("btnConfig.Image");
+            btnConfig.ImageAlign = ContentAlignment.MiddleLeft;
+            btnConfig.Location = new Point(0, 290);
+            btnConfig.Name = "btnConfig";
+            btnConfig.Padding = new Padding(12, 0, 0, 0);
+            btnConfig.Size = new Size(220, 60);
+            btnConfig.TabIndex = 3;
+            btnConfig.Text = "   Configuración";
+            btnConfig.TextAlign = ContentAlignment.MiddleLeft;
+            btnConfig.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnConfig.UseVisualStyleBackColor = true;
+            btnConfig.Click += btnConfig_Click;
+            btnConfig.MouseLeave += btnConfig_MouseLeave;
+            btnConfig.MouseMove += btnConfig_MouseMove;
+            // 
+            // btnPacientes
+            // 
+            btnPacientes.FlatAppearance.BorderSize = 0;
+            btnPacientes.FlatStyle = FlatStyle.Flat;
+            btnPacientes.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPacientes.Image = Properties.Resources._1446930__1___1_;
+            btnPacientes.ImageAlign = ContentAlignment.MiddleLeft;
+            btnPacientes.Location = new Point(0, 170);
+            btnPacientes.Name = "btnPacientes";
+            btnPacientes.Padding = new Padding(12, 0, 0, 0);
+            btnPacientes.Size = new Size(220, 60);
+            btnPacientes.TabIndex = 1;
+            btnPacientes.Text = "   Pacientes";
+            btnPacientes.TextAlign = ContentAlignment.MiddleLeft;
+            btnPacientes.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnPacientes.UseVisualStyleBackColor = true;
+            btnPacientes.Click += btnPacientes_Click;
+            btnPacientes.MouseLeave += btnPacientes_MouseLeave;
+            btnPacientes.MouseMove += btnPacientes_MouseMove;
+            // 
+            // btnConsultas
+            // 
+            btnConsultas.FlatAppearance.BorderSize = 0;
+            btnConsultas.FlatStyle = FlatStyle.Flat;
+            btnConsultas.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnConsultas.Image = (Image)resources.GetObject("btnConsultas.Image");
+            btnConsultas.ImageAlign = ContentAlignment.MiddleLeft;
+            btnConsultas.Location = new Point(0, 230);
+            btnConsultas.Name = "btnConsultas";
+            btnConsultas.Padding = new Padding(12, 0, 0, 0);
+            btnConsultas.Size = new Size(220, 60);
+            btnConsultas.TabIndex = 2;
+            btnConsultas.Text = "   Concultas";
+            btnConsultas.TextAlign = ContentAlignment.MiddleLeft;
+            btnConsultas.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnConsultas.UseVisualStyleBackColor = true;
+            btnConsultas.Click += btnConsultas_Click;
+            btnConsultas.MouseLeave += btnConsultas_MouseLeave;
+            btnConsultas.MouseMove += btnConsultas_MouseMove;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(995, 519);
+            Controls.Add(panelMenu);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)ptbLogo).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelMenu;
+        private Button btnPacientes;
+        private Button btnConfig;
+        private Button btnConsultas;
+        private PictureBox ptbLogo;
     }
 }
--- dental-C#-SQLServer-app/Form1.cs
+++ dental-C#-SQLServer-app/Form1.cs
@@ -6,5 +6,60 @@ namespace dental_C__SQLServer_app
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelLogo_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnPacientes_Click(object sender, EventArgs e)
+        {
+            btnPacientes.BackColor = Color.FromArgb(0, 255, 183);
+        }
+
+        private void btnConsultas_Click(object sender, EventArgs e)
+        {
+            btnConsultas.BackColor = Color.FromArgb(0, 255, 183);
+        }
+
+        private void btnConfig_Click(object sender, EventArgs e)
+        {
+            btnConfig.BackColor = Color.FromArgb(0, 255, 183);
+        }
+
+        private void btnPacientes_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnPacientes.BackColor = Color.FromArgb(185, 255, 233);
+        }
+
+        private void btnPacientes_MouseLeave(object sender, EventArgs e)
+        {
+            btnPacientes.BackColor = Color.Transparent;
+        }
+
+        private void btnConsultas_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnConsultas.BackColor = Color.FromArgb(185, 255, 233);
+        }
+
+        private void btnConsultas_MouseLeave(object sender, EventArgs e)
+        {
+            btnConsultas.BackColor = Color.Transparent;
+        }
+
+        private void btnConfig_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnConfig.BackColor = Color.FromArgb(185, 255, 233);
+        }
+
+        private void btnConfig_MouseLeave(object sender, EventArgs e)
+        {
+            btnConfig.BackColor = Color.Transparent;
+        }
     }
 }
