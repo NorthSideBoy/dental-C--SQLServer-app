eea0d0a574fef287833108e89a27f3ef2a967ce6
SamiraE21 <samiraestefany24@gmail.com>
2025-01-30 00:46:49 -0400

    Mostrar y registrar pacientes

---
+++ dental-C#-SQLServer-app/Clases/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace dental_C__SQLServer_app.Clases
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            textTelefono = new TextBox();
+            textSexo = new TextBox();
+            textEdad = new TextBox();
+            textFechaDeNacimiento = new TextBox();
+            textDirección = new TextBox();
+            textCédula = new TextBox();
+            textApellido = new TextBox();
+            textNombre = new TextBox();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label10 = new Label();
+            dtGridViewpacientes = new DataGridView();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dtGridViewpacientes).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(70, 117);
+            label1.Name = "label1";
+            label1.Size = new Size(355, 39);
+            label1.TabIndex = 0;
+            label1.Text = "Registro De Pacientes";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textTelefono);
+            groupBox1.Controls.Add(textSexo);
+            groupBox1.Controls.Add(textEdad);
+            groupBox1.Controls.Add(textFechaDeNacimiento);
+            groupBox1.Controls.Add(textDirección);
+            groupBox1.Controls.Add(textCédula);
+            groupBox1.Controls.Add(textApellido);
+            groupBox1.Controls.Add(textNombre);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(12, 179);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(530, 345);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Datos";
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(317, 304);
+            button3.Name = "button3";
+            button3.Size = new Size(81, 23);
+            button3.TabIndex = 18;
+            button3.Text = "Eliminar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(181, 304);
+            button2.Name = "button2";
+            button2.Size = new Size(81, 23);
+            button2.TabIndex = 17;
+            button2.Text = "Modificar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(58, 304);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 10;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textTelefono
+            // 
+            textTelefono.Location = new Point(81, 217);
+            textTelefono.Name = "textTelefono";
+            textTelefono.Size = new Size(159, 33);
+            textTelefono.TabIndex = 16;
+            // 
+            // textSexo
+            // 
+            textSexo.Location = new Point(365, 217);
+            textSexo.Name = "textSexo";
+            textSexo.Size = new Size(140, 33);
+            textSexo.TabIndex = 15;
+            // 
+            // textEdad
+            // 
+            textEdad.Location = new Point(365, 152);
+            textEdad.Name = "textEdad";
+            textEdad.Size = new Size(140, 33);
+            textEdad.TabIndex = 14;
+            // 
+            // textFechaDeNacimiento
+            // 
+            textFechaDeNacimiento.Location = new Point(365, 93);
+            textFechaDeNacimiento.Name = "textFechaDeNacimiento";
+            textFechaDeNacimiento.Size = new Size(140, 33);
+            textFechaDeNacimiento.TabIndex = 13;
+            // 
+            // textDirección
+            // 
+            textDirección.Location = new Point(81, 152);
+            textDirección.Name = "textDirección";
+            textDirección.Size = new Size(159, 33);
+            textDirección.TabIndex = 12;
+            // 
+            // textCédula
+            // 
+            textCédula.Location = new Point(81, 93);
+            textCédula.Name = "textCédula";
+            textCédula.Size = new Size(154, 33);
+            textCédula.TabIndex = 9;
+            textCédula.TextChanged += textBox1_TextChanged;
+            // 
+            // textApellido
+            // 
+            textApellido.Location = new Point(365, 32);
+            textApellido.Name = "textApellido";
+            textApellido.Size = new Size(140, 33);
+            textApellido.TabIndex = 10;
+            // 
+            // textNombre
+            // 
+            textNombre.Location = new Point(81, 30);
+            textNombre.Name = "textNombre";
+            textNombre.Size = new Size(159, 33);
+            textNombre.TabIndex = 11;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(284, 227);
+            label9.Name = "label9";
+            label9.Size = new Size(37, 17);
+            label9.TabIndex = 7;
+            label9.Text = "Sexo";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(14, 227);
+            label8.Name = "label8";
+            label8.Size = new Size(62, 17);
+            label8.TabIndex = 6;
+            label8.Text = "Teléfono";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(284, 162);
+            label7.Name = "label7";
+            label7.Size = new Size(42, 17);
+            label7.TabIndex = 5;
+            label7.Text = "Edad";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(14, 162);
+            label6.Name = "label6";
+            label6.Size = new Size(69, 17);
+            label6.TabIndex = 4;
+            label6.Text = "Dirección";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(14, 103);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 17);
+            label5.TabIndex = 3;
+            label5.Text = "Cédula";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(284, 109);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 17);
+            label4.TabIndex = 2;
+            label4.Text = "F. De Nac.";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(284, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 17);
+            label3.TabIndex = 1;
+            label3.Text = "Apellido";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(14, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 17);
+            label2.TabIndex = 0;
+            label2.Text = "Nombre";
+            label2.Click += label2_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Century Gothic", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(850, 117);
+            label10.Name = "label10";
+            label10.Size = new Size(297, 39);
+            label10.TabIndex = 8;
+            label10.Text = "Lista de Pacientes";
+            // 
+            // dtGridViewpacientes
+            // 
+            dtGridViewpacientes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtGridViewpacientes.Location = new Point(678, 190);
+            dtGridViewpacientes.Name = "dtGridViewpacientes";
+            dtGridViewpacientes.Size = new Size(615, 414);
+            dtGridViewpacientes.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1305, 616);
+            Controls.Add(dtGridViewpacientes);
+            Controls.Add(label10);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dtGridViewpacientes).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox textTelefono;
+        private TextBox textSexo;
+        private TextBox textEdad;
+        private TextBox textFechaDeNacimiento;
+        private TextBox textDirección;
+        private TextBox textCédula;
+        private TextBox textApellido;
+        private TextBox textNombre;
+        private DataGridView dtGridViewpacientes;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+    }
+}
---
+++ dental-C#-SQLServer-app/Clases/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dental_C__SQLServer_app.Clases
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CConexion.Conectar();
+            dtGridViewpacientes.DataSource = Index();
+        }
+
+        public DataTable Index()
+        {
+            DataTable dataTable = new DataTable();
+            string Sql = "SELECT * FROM pacientes";
+            SqlCommand CMD = new SqlCommand(Sql, CConexion.Conectar());
+            SqlDataAdapter adapter = new SqlDataAdapter(CMD);
+
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CConexion.Conectar();
+
+            string Guardar = "INSERT INTO pacientes (Nombre,Apellido,Cédula,FechaDeNacimiento,Dirección,Edad,Telefono,Sexo) VALUES (@Nombre,@Apellido,@Cédula,@FechaDeNacimiento,@Dirección,@Edad,@Telefono,@Sexo)";
+            SqlCommand insert = new SqlCommand(Guardar, CConexion.Conectar());
+            insert.Parameters.AddWithValue("@Nombre", textNombre.Text);
+            insert.Parameters.AddWithValue("@Apellido", textApellido.Text);
+            insert.Parameters.AddWithValue("@Cédula", textCédula.Text);
+            insert.Parameters.AddWithValue("@FechaDeNacimiento", textFechaDeNacimiento.Text);
+            insert.Parameters.AddWithValue("@Dirección", textDirección.Text);
+            insert.Parameters.AddWithValue("@Edad", textEdad.Text);
+            insert.Parameters.AddWithValue("@Telefono", textTelefono.Text);
+            insert.Parameters.AddWithValue("@Sexo", textSexo.Text);
+
+            insert.ExecuteNonQuery();
+
+            MessageBox.Show("Los Datos Fueron Guardados Correctamente");
+
+            dtGridViewpacientes.DataSource = Index();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
